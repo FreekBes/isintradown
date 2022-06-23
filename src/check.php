7199f9a134ed4ec58f9b316360b3bdeebc78844d
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-06-24 00:01:25 +0200

    save avg response time to history and cleanup

--- src/check.php
+++ src/check.php
@@ -1,16 +1,13 @@
 #!/usr/bin/php
 <?php
-require_once("config.php");
-require_once("token.php");
+require_once(dirname(__FILE__)."/config.php");
+require_once(dirname(__FILE__)."/token.php");
 
-$file_name = "../history.json";
+$file_name = dirname(__FILE__)."/../history.json";
 if (!file_exists($file_name))
 	$status_hist = array();
 else
-{
-	$status_hist = file_get_contents($file_name);
-	$status_hist = json_decode($status_hist, true);
-}
+	$status_hist = json_decode(file_get_contents($file_name), true);
 
 $data = get_token(array());
 
@@ -41,9 +38,7 @@ function is_up($group_id)
 			if (array_key_exists("error", $json))
 				$status["online"] = false;
 			else
-			{
 				$status["online"] = true;
-			}
 		}
 		catch (Exception $e) { $status["online"] = false; }
 	}
@@ -52,27 +47,33 @@ function is_up($group_id)
 		$status["online"] = false; //curl_exec errored
 		$status["res_time"] = -1;
 	}
-	file_put_contents("../status.json", json_encode($status, JSON_PRETTY_PRINT));
+	file_put_contents(dirname(__FILE__)."/../status.json", json_encode($status, JSON_PRETTY_PRINT));
 	return ($status);
 }
 
 $group_id = 0;
 $counter = 0;
+$avg_res_time = 0;
+$sleep_time = 10; //sleep for this amount of seconds after every check
+$save_every_runs = 30; //save status to history every 30 checks (+/- every 5 minutes)
 
 while (1)
 {
 	$status = is_up($group_id);
 	$counter++;
 	$group_id++;
+	$avg_res_time += ($status["online"] ? $status["res_time"] : 0);
 	if ($group_id > 69)
 		$group_id = 0;
-	if ($counter == 30) //saves every 5 min (30 runs)
+	if ($counter == $save_every_runs) //saves every so many runs
 	{
-		array_unshift($status_hist, $status);
+		$status["res_time"] = round($avg_res_time / $save_every_runs); //replace res_time fetched with the average response time
+		array_unshift($status_hist, $status); //add to front of history array
 		array_slice($status_hist, 0, 864, false); //864 == amount of 5 minute segments in 3 days
 		file_put_contents($file_name, json_encode($status_hist, JSON_PRETTY_PRINT)); //saves last 3 days of statuses
 		$counter = 0;
+		$avg_res_time = 0; //reset average response time
 	}
-	sleep(10);
+	sleep($sleep_time);
 }
 ?>
